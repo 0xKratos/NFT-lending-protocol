8ac5556b6145c2f63a651db2f5480fe1610b7f8c
Dawg679 <tanjunmeng@gmail.com>
2022-06-03 21:43:52 +0800

    feat: :sparkles: Add borrow function

---
+++ contracts/NFTLendingPool.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "./interface/INFTLendingPool.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFTLendingPool is INFTLendingPool, ERC721Holder, ReentrancyGuard, Ownable
+
+{
+    // Variables
+    struct Loan {
+        address borrower;
+        address collateralCollectionAddress;
+        uint256 collateralTokenId;
+        uint256 collateralPrice;
+        uint256 principal;
+        uint256 startTime;
+        uint256 endTime;
+    }
+
+
+
+    uint256 public interestRateMolecular;
+    uint256 public interestRateDenominator;
+    IERC20 public USDC;
+    uint256 public maxLoanMolecular = 7;
+    uint256 public maxLoanDenominator = 10;
+    uint256 public loanPeriod = 1 days;
+
+
+    mapping(uint256 => Loan) public loans;
+    uint256 public loanCounter = 0;
+    event Borrow(address indexed borrower, uint256 indexed loanId, uint256 amountBorrowed, uint256 startTime, uint256 endTime);
+    event Repay();
+    event Liquidate();
+
+    // Functions
+    constructor(address _usdc, uint256 _interestRateMolecular, uint256 _interestRateDenominator){
+        interestRateMolecular = _interestRateMolecular;
+        interestRateDenominator = _interestRateDenominator;
+        USDC = IERC20(_usdc);
+    }
+    
+    function borrow(uint256 _amount, IERC721 _collectionAddress, uint256 _id, uint256 _price) external {
+        _addCollateral(_collectionAddress, _id);
+        require(_amount * maxLoanDenominator <= _price  * maxLoanMolecular, "NFTLendingPool: Principal must be less than maximum loan amount");
+        USDC.safeTransfer(msg.sender,_amount);
+        loans[loanCounter] = Loan(msg.sender, _collectionAddress, _id, _price, _amount, block.timestamp, block.timestamp+loanPeriod);
+        loanCounter++;
+        emit Borrow(msg.sender, _id, _amount, block.timestamp, block.timestamp+loanPeriod);
+
+    }
+    function repay(uint256 _loanId) external;
+    function liquidate(uint256 _loanId) external;
+    function canLiquidate(uint256 _loanId) external view returns (bool);
+    function _addCollateral(IERC721 _collectionAddress, uint256 _id) private {
+        _collectionAddress.safeTransferFrom(msg.sender,address(this),_id);
+    }
+    function _removeCollateral(IERC721 _collectionAddress, uint256 _id) private{
+        _collectionAddress.safeTransferFrom(address(this),msg.sender,_id);
+    }
+    function _calculateRepayment(Loan loan) private returns (uint256);
+
+    function _calculateTimePassed(uint256 startTime) private returns(uint256);
+
+
+}
--- contracts/interface/INFTLendingPool.sol
+++ contracts/interface/INFTLendingPool.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
 
-interface INFTLendingProtocol {
+interface INFTLendingPool {
 
     struct Loan {
-        address borrower
-        address collateralCollectionAddress:
-        uint256 collateralTokenId:
-        uint256 collateralPrice:
-        uint256 principal:
-        uint256 startTime:
-        uint256 endTime
-        uint256 isRepayed
+        address borrower;
+        address collateralCollectionAddress;
+        uint256 collateralTokenId;
+        uint256 collateralPrice;
+        uint256 principal;
+        uint256 startTime;
+        uint256 endTime;
+        uint256 isRepayed;
     }
     function borrow(uint256 _amount, ERC721 _collectionAddress, uint256 _id, uint256 _price) external;
     function repay(uint256 _loanId) external;
