9a2c23d756c17b885e8518b786004fc150bbbc87
Dawg679 <tanjunmeng@gmail.com>
2022-06-04 15:25:33 +0800

    feat: :sparkles: Add NFT minting

--- contracts/MockERC721.sol
+++ contracts/MockERC721.sol
@@ -5,4 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MockERC721 is ERC721 {
     constructor() ERC721("Mock", "Mock") {}
+    function mint(address _to, uint256 _tokenId) public {
+        _mint(_to, _tokenId);
+    }
 }
