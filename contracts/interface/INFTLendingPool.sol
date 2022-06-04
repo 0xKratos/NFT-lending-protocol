866f43326ed295902cd8dcd293c56d64cf89bb98
Dawg679 <tanjunmeng@gmail.com>
2022-06-04 12:11:02 +0800

    refactor: :recycle: Refactor interface

--- contracts/interface/INFTLendingPool.sol
+++ contracts/interface/INFTLendingPool.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.11;
-
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 interface INFTLendingPool {
 
     struct Loan {
@@ -11,11 +11,10 @@ interface INFTLendingPool {
         uint256 principal;
         uint256 startTime;
         uint256 endTime;
-        uint256 isRepayed;
     }
-    function borrow(uint256 _amount, ERC721 _collectionAddress, uint256 _id, uint256 _price) external;
+    function borrow(uint256 _amount, IERC721 _collectionAddress, uint256 _id, uint256 _price) external;
     function repay(uint256 _loanId) external;
     function liquidate(uint256 _loanId) external;
-    function canLiquidate(uint256 _loanId) external view returns (bool)
+    function canLiquidate(uint256 _loanId) external view returns (bool);
 
 }
